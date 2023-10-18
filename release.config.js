d46dc49288fd08f54355f3d5d8530108c3373468
Johannes Stang <tubenhirn@gmail.com>
2023-10-18 10:00:42 +0200

    fix: update 1123

--- release.config.js
+++ release.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   branches: [
     { name: "main", channel: "latest" },
-    { name: "hotfix/*"},
+    { name: "hotfix/*", prerelease: '${name.replace(/^hotfix\\//g, "")}'},
   ],
   plugins: [
     "@semantic-release/commit-analyzer",
