4a90803167baf58ef562651f48663fe984d78608
Johannes Stang <tubenhirn@gmail.com>
2023-10-18 08:58:24 +0200

    ci: update

--- .releaserc.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  "branches": ['+([0-9])?(.{+([0-9]),x}).x', "main", "release", {name: "hotfix/*", prerelease: "hotfix", channel: "hotfix"}],
-  "plugins": ['@semantic-release/commit-analyzer', '@semantic-release/release-notes-generator', '@semantic-release/github']
-};
